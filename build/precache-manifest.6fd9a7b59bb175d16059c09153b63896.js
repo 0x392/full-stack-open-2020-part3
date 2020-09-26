3bc3b0b10e0a8c008f1791f21157071ab53cd718
Ryan <70819971+0x392@users.noreply.github.com>
2020-09-26 11:27:07 +0800

    Create build directory
    The production build of the frontend

---
+++ build/precache-manifest.6fd9a7b59bb175d16059c09153b63896.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc41f33851db012be2f19253c3e306a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bf6794492354b40e90",
+    "url": "/static/js/main.189fbe0d.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
