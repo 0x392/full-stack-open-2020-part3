878410a3ec34b000870595c6154210e28e5cf037
Ryan <70819971+0x392@users.noreply.github.com>
2020-09-30 12:24:41 +0800

    Update index.js
    To exercises 3.18* (end of part 3c)

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
+const Person = require("./models/Person");
 
 const app = express();
-
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
 
 morgan.token("request-content", function (request, response) {
   return JSON.stringify(request.body);
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "mal-formatted id" });
+  }
+
+  next(error);
+};
+
+app.use(express.static("build"));
+app.use(express.json());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :request-content"
   )
 );
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/info", (request, response) => {
   const info = `Phonebook has info for ${persons.length} people`;
@@ -47,23 +34,27 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => response.json(persons));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id == id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id != id);
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -73,17 +64,32 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({ error: "the name is missing" });
   if (!body.number)
     return response.status(400).json({ error: "the number is missing" });
-  if (persons.find((person) => person.name == body.name))
-    return response.status(400).json({ error: "the name already exists" });
 
-  const person = {
-    id: Math.floor(Math.random() * 1e6),
-    name: body.name,
-    number: body.number,
-  };
-  persons = persons.concat(person); // Why not just use `.push(..)`?
-  response.json(person);
+  Person.find({ name: body.name }).then((result) => {
+    if (result.length !== 0) {
+      return response.status(400).json({ error: "the name already exists" });
+    } else {
+      const person = new Person({
+        name: body.name,
+        number: body.number,
+      });
+      person.save().then((savedPerson) => response.json(savedPerson));
+    }
+  });
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = { number: body.important };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
