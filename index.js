b7b0f96932c61eed41b6bb24b2679a791f2188b2
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-02 05:30:58 +0800

    Update index.js and Person.js
    To pass linting

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const Person = require("./models/Person");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const Person = require('./models/Person');
 
 const app = express();
 
-morgan.token("request-content", function (request, response) {
-  return JSON.stringify(request.body);
-});
+morgan.token('request-content', (request, _response) => JSON.stringify(request.body));
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "mal-formatted id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'mal-formatted id' });
+  }
+  if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message });
   }
 
   next(error);
 };
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 app.use(express.json());
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :request-content"
-  )
+    ':method :url :status :res[content-length] - :response-time ms :request-content',
+  ),
 );
 
-app.get("/info", (request, response) => {
-  const info = `Phonebook has info for ${persons.length} people`;
-  const time = new Date().toUTCString();
-  response.send(`${info}<br>${time}`);
-});
-
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => response.json(persons));
 });
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -51,21 +44,23 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
+    .then((_result) => {
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const { body } = request;
 
-  if (!body.name)
-    return response.status(400).json({ error: "the name is missing" });
-  if (!body.number)
-    return response.status(400).json({ error: "the number is missing" });
+  if (!body.name) {
+    return response.status(400).json({ error: 'the name is missing' });
+  }
+  if (!body.number) {
+    return response.status(400).json({ error: 'the number is missing' });
+  }
 
   const person = new Person({
     name: body.name,
@@ -77,8 +72,8 @@ app.post("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { body } = request;
   const person = { number: body.number };
 
   Person.findByIdAndUpdate(request.params.id, person, {
